127548c5bd82024e84a2c22fe1012e439a08bf4d
Lorin Steel <lorinsteel@gmail.com>
2016-02-09 09:50:04 -0700

    Commiting the second part of the assignment

--- app.js
+++ app.js
@@ -3,18 +3,89 @@ var express = require('express'),
 
 var app = express();
 
-reps.allByZip('84043', function(err, results) {
-  results.forEach(function(key) {
-    console.log(key.name);
-  });
-});
-
 app.get('/', function(req, res) {
   res
     .status(200)
     .send('Welcome to my api!');
 });
 
-app.listen(8000, function() {
-  console.log('Listening on port 8000.');
+app.get('/api/all/by-zip/:zip', function(req, res, next) {
+  reps.allByZip(req.params.zip, function(err, results) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else if (results) {
+      res.send(results);
+      next();
+    }
+    else {
+      res.status(404).send('Zip not found.');
+    }
+  });
 });
+
+app.get('/api/reps/by-name/:lastName', function(req, res, next) {
+  reps.repsByName(req.params.lastName, function(err, results) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else if (results) {
+      res.send(results);
+      next();
+    }
+    else {
+      res.status(404).send('Last name not found.');
+    }
+  });
+});
+
+app.get('/api/reps/by-state/:state', function(req, res, next) {
+  reps.repsByState(req.params.state, function(err, results) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else if (results) {
+      res.send(results);
+      next();
+    }
+    else {
+      res.status(404).send('State not found.');
+    }
+  });
+});
+
+app.get('/api/sens/by-name/:lastName', function(req, res, next) {
+  reps.sensByName(req.params.lastName, function(err, results) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else if (results) {
+      res.send(results);
+      next();
+    }
+    else {
+      res.status(404).send('Last name not found.');
+    }
+  });
+});
+
+app.get('/api/sens/by-state/:state', function(req, res, next) {
+  reps.sensByState(req.params.state, function(err, results) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else if (results) {
+      res.send(results);
+      next();
+    }
+    else {
+      res.status(404).send('State not found.');
+    }
+  });
+});
+
+app.listen(8000, function() {
+  console.log('Listening on port 8000 :P');
+});
+
+module.exports = app;
